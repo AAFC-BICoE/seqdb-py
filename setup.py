dc36307489da91c89ecf1831e567f8c316890b10
okorol <okorol@gmail.com>
2015-04-27 16:00:59 -0400

    MANIFEST and setup.py, required to create a mudule distribution of this project

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#!/usr/bin/python
+'''
+Created on Apr 27, 2015
+
+@author: korolo
+'''
+
+from distutils.core import setup
+
+setup(name='seqdb-py',
+      version='1.0',
+      description='SeqDB Python Extensions',
+      author='Oksana Korol',
+      author_email='oksana.korol@agr.gc.ca',
+      url='https://bitbucket.org/aafc-mbb/seqdb-py',
+      packages=['api', 'tools'],
+     )
