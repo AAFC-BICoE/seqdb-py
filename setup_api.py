ab400e10e605581b75e6256184264f89b818e202
okorol <okorol@gmail.com>
2015-04-30 14:14:39 -0400

    Added ability to install api separately

--- setup_api.py
+++ setup_api.py
@@ -9,19 +9,16 @@ Created on Apr 27, 2015
 #from distutils.core import setup
 from setuptools import setup
 
-setup(name='seqdb-py',
+setup(name='seqdb-py-api',
     version='1.0',
-    description='SeqDB Python Extensions: API and tools',
+    description='SeqDB Python API.',
     author='Oksana Korol',
     author_email='oksana.korol@agr.gc.ca',
     url='https://bitbucket.org/aafc-mbb/seqdb-py',
-    packages=['api', 'tools'],
+    packages=['api'],
     #data_files=[('', 'requirements.txt')],
     install_requires = [
-        'biopython==1.65',
-        'logilab-common==0.63.2',
         'PyYAML==3.11',
         'requests==2.6.0', 
-        'six==1.9.0',
      ]
 )
