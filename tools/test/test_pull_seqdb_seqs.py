08be2556edbf2b56467613d4c6745eec20fdb957
Cime Ajouz <cime.ajouz@hotmail.ca>
2016-03-09 14:43:52 -0500

    Feature #6603: Add test for sequence file creation [Issue URL: http://redmine.biodiversity.agr.gc.ca/issues/6603] 	- Expanded test for pull_seqdb_seqs.execute_script to check for: 		* file size 		* whether expected header lines of sequences match the ones in the output file 		* non existing files

--- tools/test/test_pull_seqdb_seqs.py
+++ tools/test/test_pull_seqdb_seqs.py
@@ -62,18 +62,75 @@ class TestPullSeqdbSeqs(unittest.TestCase):
         self.assertEquals(480088, len(seq_ids), "Expected 480,088 raw sequences, but got %i. Doublecheck test db to make sure the numbers haven't changed there." % len(seq_ids))
     """    
     def test_execute_script_consensus(self):
-        # time: 124.221s
+        # time: 127.712s
+        
+        # Testing fasta file creation for consensus sequences 
         pull_seqdb_seqs.execute_script(["-c", config_root.path() + '/config4tests.yaml', "-r", "fasta", "consensus"], 
                              self.output_file_name, self.output_taxon_file_name)
         self.assertTrue(os.path.isfile(self.output_fasta_file_name), "Fasta file was not created.")
+        self.assertFalse(os.path.isfile(self.output_fastq_file_name), "Fastq file was created.")
+        self.assertFalse(os.path.isfile(self.output_taxon_file_name), "Taxonomy file was created.")
+        self.assertEqual(13983492, os.stat(self.output_fasta_file_name).st_size, "File size expected to be 13983492 bytes, but is %i" %os.stat(self.output_fasta_file_name).st_size)
+        output_file = open(self.output_fasta_file_name)
+        actual_lines = output_file.readlines()
+        expected_line_0 = '>seqdb|358301 Pythium arrhenomanes Pyt_arrhenomanes_BR1028_ACA \n'
+        self.assertEqual(expected_line_0, actual_lines[0], "Expected line is not the same as the actual line.")
+        expected_line_89597 = '>seqdb|4823203 Couesius plumbeus NXG2013358A_RHO \n'
+        self.assertEqual(expected_line_89597, actual_lines[89597], "Expected line is not the same as the actual line.")
+        expected_line_161610 = '>seqdb|4829279   12G4130_GRSPaV_partial \n'
+        self.assertEqual(expected_line_161610, actual_lines[161610], "Expected line is not the same as the actual line.")
         
     def test_execute_script_raw(self):
-        # time: 13.751s
-        # time with optimization: 5.143s
+        # time: 28.616
+        
+        # Testing fasta file creation for raw sequences 
         pull_seqdb_seqs.execute_script(["-c", config_root.path() + '/config4tests.yaml', "-r", "fasta", "raw", "--seqName", "S-SH-"], 
-                             self.output_file_name, self.output_taxon_file_name)
+                            self.output_file_name, self.output_taxon_file_name)
         self.assertTrue(os.path.isfile(self.output_fasta_file_name), "Fasta file was not created.")
-
+        self.assertFalse(os.path.isfile(self.output_taxon_file_name), "Taxonomy file was created.")
+        self.assertEqual(138996, os.stat(self.output_fasta_file_name).st_size, "File size expected to be 138996 bytes, but is %i" %os.stat(self.output_fasta_file_name).st_size)
+        output_file = open(self.output_fasta_file_name)
+        actual_lines = output_file.readlines()
+        expected_line_0 = '>seqdb|1 G07_S-SH-27_R25_its4_3.ab1 \n'
+        self.assertEqual(expected_line_0, actual_lines[0], "Expected line is not the same as the actual line.")
+        expected_line_1026 = '>seqdb|79390 D08_S-SH-98_RS13_LROR_4.ab1 \n'
+        self.assertEqual(expected_line_1026, actual_lines[1026], "Expected line is not the same as the actual line.")
+        expected_line_1838 = '>seqdb|126059 B01_S-SH-36_M2_its4a_2.ab1 \n'
+        self.assertEqual(expected_line_1838, actual_lines[1838], "Expected line is not the same as the actual line.")
+        
+        # Testing fastq file creation for raw sequences 
+        pull_seqdb_seqs.execute_script(["-c", config_root.path() + '/config4tests.yaml', "-r", "fastq", "raw", "--sampleName", "LEV6103"], 
+                             self.output_file_name, self.output_taxon_file_name)
+        self.assertTrue(os.path.isfile(self.output_fastq_file_name), "Fastq file was not created.")
+        self.assertFalse(os.path.isfile(self.output_taxon_file_name), "Taxonomy file was created.")
+        self.assertEqual(106623, os.stat(self.output_fastq_file_name).st_size, "File size expected to be 106623 bytes, but is %i" %os.stat(self.output_fastq_file_name).st_size)
+        output_file = open(self.output_fastq_file_name)
+        actual_lines = output_file.readlines()
+        expected_line_0 = '@seqdb|266400 Myzocytiopsis ? sp. affin. intermedia AL_HM_H047_09_SEQ_LEV6103_18S_NS1\n'
+        self.assertEqual(expected_line_0, actual_lines[0], "Expected line is not the same as the actual line.")
+        expected_line_140 = '@seqdb|301609 Myzocytiopsis ? sp. affin. intermedia AL_TR_T126_41_SEQ_LEV6103A_29_TF_M13_LO\n'
+        self.assertEqual(expected_line_140, actual_lines[140], "Expected line is not the same as the actual line.")
+        expected_line_228 = '@seqdb|331086 Myzocytiopsis ? sp. affin. intermedia AL_HM_H080_25_SEQ_LEV6103_BTUB_OOM-BTUB-LO-1401\n'
+        self.assertEqual(expected_line_228, actual_lines[228], "Expected line is not the same as the actual line.")
+        
+    def test_execute_script_all(self):
+        # time: 72.028s
+        
+        # Testing fasta file creation for all sequences
+        pull_seqdb_seqs.execute_script(["-c", config_root.path() + '/config4tests.yaml', "-r", "fasta", "all", "--geneRegion", "ACA"], 
+                            self.output_file_name, self.output_taxon_file_name)
+        self.assertTrue(os.path.isfile(self.output_fasta_file_name), "Fasta file was not created.")
+        self.assertFalse(os.path.isfile(self.output_fastq_file_name), "Fastq file was created.")
+        self.assertFalse(os.path.isfile(self.output_taxon_file_name), "Taxonomy file was created.")
+        output_file = open(self.output_fasta_file_name)
+        actual_lines = output_file.readlines()
+        expected_line_0 = '>seqdb|6818 Pythium irregulare G079_08_BR1000_ACA_ACAUP16 \n'
+        self.assertEqual(expected_line_0, actual_lines[0], "Expected line is not the same as the actual line.")
+        expected_line_7794 = '>seqdb|112673 Pythium dimorphum T041_014_M0236_ACA_ACAUP829PP \n'
+        self.assertEqual(expected_line_7794, actual_lines[7794], "Expected line is not the same as the actual line.")
+        expected_line_14604 = '>seqdb|358505 Phytophthora porri Phy_porri_Lev1964_ACA \n'
+        self.assertEqual(expected_line_14604, actual_lines[14604], "Expected line is not the same as the actual line.")
+        
              
     def test_get_seq_ids_specimen(self):  
         # time: 13.833s
