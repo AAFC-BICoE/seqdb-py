19a9c86f65e51ab8d9d46d65541a14c0e82f6e5a
oxyko <okorol@gmail.com>
2016-04-06 13:37:50 -0400

    Refactored SeqSourceApi.

---
+++ api/SeqSourceApi.py
@@ -0,0 +1,19 @@
+'''
+Created on April 06, 2016
+
+@author: Oksana Korol
+
+Class that extracts common functionality for all SeqDB API entities
+'''
+
+from api.BaseApiEntity import BaseApiEntity
+
+class SeqSourceApi(BaseApiEntity):
+
+    def __init__(self, api_key, base_url, sequence_id):
+        self.clearAllFilters()
+        super(SeqSourceApi, self).__init__(api_key=api_key, base_url=base_url, request_url="sequence/{}/seqSource".format(sequence_id))
+        
+        
+    def getParamsStr(self):
+        return ''
