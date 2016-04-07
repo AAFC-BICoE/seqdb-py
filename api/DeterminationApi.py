a8b6282988ee318bd211f03fd5779defce0f59e3
oxyko <okorol@gmail.com>
2016-04-07 11:55:24 -0400

    Fix to remove double slash in the request urls.

--- api/DeterminationApi.py
+++ api/DeterminationApi.py
@@ -51,7 +51,7 @@ class DeterminationApi(BaseApiEntity):
                 "evidenceNotes": notes}
             }
 
-        resp = super(DeterminationApi, self).create("{}/{}".format(self.base_url, self.request_url), json.dumps(post_data))
+        resp = super(DeterminationApi, self).create("{}{}".format(self.base_url, self.request_url), json.dumps(post_data))
         jsn_resp = resp.json()
         
         if 'result' and 'metadata' not in jsn_resp:
