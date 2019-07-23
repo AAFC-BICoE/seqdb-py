c3c40790fc6d4b6d605608cca2b0b025a51620b4
Melanie <belisleleclerc@gmail.com>
2019-07-23 08:51:47 -0400

    Fixed issue with relative paths

--- seqdb_py/api/test/context.py
+++ seqdb_py/api/test/context.py
@@ -1,7 +1,9 @@
+
 import os
 import sys
 
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 sys.path.insert(1, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from RawSequenceApi import RawSequenceApi
+from api.RawSequenceApi import RawSequenceApi
+from config import config_root
